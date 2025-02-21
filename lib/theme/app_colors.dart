17ff3798c1aa7178ab33f1d85ff1166b78ba1600
Berto <albertoredondo@paucasesnovescifp.cat>
2025-02-21 12:24:42 +0100

    Update app_colors.dart

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -7,12 +7,10 @@ class AppColors {
   static const Color accent = Color(0xFFA3FD8C);
 
   static const Color backgroundLight = Color(0xFFFEF3DD);
-  static const Color backgroundDark =
-      Color(0xFF134247); // 🔹 Color de fondo para el tema oscuro
+  static const Color backgroundDark = Color(0xFF134247); 
 
   static const Color textLight = Colors.black87;
-  static const Color textDark =
-      Color(0xFFACEDFD); // 🔹 Texto blanco en tema oscuro
+  static const Color textDark = Color(0xFFACEDFD);
 
   static const Color shadowLight = Color(0xFF134247);
   static const Color shadowDark = Colors.black54;
